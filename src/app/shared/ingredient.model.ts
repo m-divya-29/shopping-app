88647c3ecd2ea964ba27021a9ea7543662e214fb
dmahanka <dmahanka@opentext.com>
2023-06-17 22:31:54 +0530

    ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,3 @@
+export class Ingredient{
+constructor(public name: string, public amount: number) {} // this is shortcut to manually adding these attributes to the class and assigning this.name = name and this.amount = amount. 
+}
